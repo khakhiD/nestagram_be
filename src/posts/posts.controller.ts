0bc9b7f4b5f8c5366e2af553fc452b15f442533a
khakhiD <kakkiid@gmail.com>
2024-09-01 03:08:32 +0900

    feat: 비즈니스 로직을 서비스로 옮기기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,48 +3,11 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
-
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_official',
-    title: '뉴진스 민지',
-    content: '메이크업 고치고 있는 민지',
-    likeCount: 100000,
-    commentCount: 99999,
-  },
-  {
-    id: 2,
-    author: 'newjeans_official',
-    title: '뉴진스 해린',
-    content: '연습하는 해린',
-    likeCount: 100000,
-    commentCount: 99999,
-  },
-  {
-    id: 3,
-    author: 'blackpink_official',
-    title: '블랙핑크 로제',
-    content: '종합운동장에서 공연하는 로제',
-    likeCount: 100000,
-    commentCount: 99999,
-  },
-];
 
 @Controller('posts')
 export class PostsController {
@@ -53,19 +16,13 @@ export class PostsController {
   // 1) GET /posts - 모든 post를 다 가져온다.
   @Get()
   getPosts() {
-    return posts;
+    return this.postsService.getAllPosts();
   }
 
   // 2) GET /posts/:id - id에 해당하는 post를 가져온다.
   @Get(':id')
   getPost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    return post;
+    return this.postsService.getPostById(+id);
   }
 
   // 3) POST /posts - post를 생성한다.
@@ -75,18 +32,7 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const post = {
-      id: posts[posts.length - 1].id + 1,
-      author,
-      title,
-      content,
-      likeCount: 0,
-      commentCount: 0,
-    };
-
-    posts = [...posts, post];
-
-    return post;
+    return this.postsService.createPost(author, title, content);
   }
 
   // 4) PUT /posts/:id - id에 해당하는 post를 변경한다.
@@ -97,25 +43,12 @@ export class PostsController {
     @Body('title') title?: string,
     @Body('content') content?: string,
   ) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) throw new NotFoundException();
-    if (author) post.author = author;
-    if (title) post.title = title;
-    if (content) post.content = content;
-
-    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
-
-    return post;
+    return this.postsService.updatePost(+id, author, title, content);
   }
 
   // 5) DELETE /posts/:id - id에 해당하는 post를 삭제한다.
   @Delete(':id')
   deletePost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-    if (!post) throw new NotFoundException();
-
-    posts = posts.filter((post) => post.id !== +id);
-    return id;
+    this.postsService.deletePost(+id);
   }
 }
